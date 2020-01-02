04e04c64ff631a9c4d89130aecc66afbdd0720ab
Amanda Stensland <amanda.stensland@bonniernews.se>
2020-01-02 01:34:08 +0100

    update layout

---
+++ js/links.js
@@ -0,0 +1,17 @@
+const links = [
+  {
+    name: "CSS Stats",
+    url: "https://cssstats.com/",
+    description: ""
+  },
+  {
+    name: "Editor Snippet Generator",
+    url: "https://snippet-generator.app/",
+    description: "Paste'a in er kod och den spottar ur sig en snippets-definition som du kan klippa in i din editor-snippets config."
+  },
+  {
+    name: "Neal",
+    url: "https://neal.fun/",
+    description: "Bringing back the wierd web."
+  }
+];
